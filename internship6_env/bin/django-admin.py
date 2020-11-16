34175f8f4b938f6a23e79968d70a0f0a02ecfa65
Aktar Zaman <aktarzaman@berkeley.edu>
2020-11-15 21:44:00 -0500

    first commit

---
+++ internship6_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zamy/Desktop/Python_Projects/excl_intrnship_projects/excl_internship_0/internship_6/internship_6/internship6_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ internship6_env/bin/rst2man.py
@@ -0,0 +1,26 @@
+#!/Users/zamy/Desktop/Python_Projects/excl_intrnship_projects/excl_internship_0/internship_6/internship_6/internship6_env/bin/python3
+
+# Author:
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  " + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
